3cbd37075fe3388222435abcd5032e6003fc2aef
Arnov2837 <Shohanurroshid2@gmail.com>
2026-07-18 06:23:40 +0300

    final

--- src/Component/Country/Countris.jsx
+++ src/Component/Country/Countris.jsx
@@ -6,16 +6,29 @@ const Countris = ({CountryPromis}) => {
   const Countriss =use(CountryPromis);
   const Countris =Countriss.countries;
   // console.log(Countris);
+
+  const[visitedCountry,setvisitedCountry] = useState([]);
+  const handele = (Countryname) =>{
+    console.log('Countrynameparci','Countryname');
+    const NewVisCountry = [...visitedCountry,Countryname];
+    setvisitedCountry(NewVisCountry);
+    
+  }
   
   return (
     <div>
       <h1>all countrys: {Countris.length}</h1>
+
+      <h3>visited Country:{visitedCountry.length}</h3>
        <div className='grd'>
-         {
-        Countris.map(Countryname => <CountryName
+         { 
+           Countris.map(Countryname => <CountryName
            key={Countryname.cca3.cca3}
-            Countryname={Countryname}></CountryName>)
-      }
+            Countryname={Countryname}
+              handele ={handele}>
+            </CountryName>)
+           
+          }
        </div>
      
     </div>
--- src/Component/CountryName/CountryName.jsx
+++ src/Component/CountryName/CountryName.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CountryName.css';
 
-const CountryName = ({Countryname}) => {
+
+
+
+const CountryName = ({Countryname,handele}) => {
+
+
+  const [visit , setvisit] = useState(false);
   // console.log(CountryName);
-  const visit =() =>{
-    console.log('clic kkorci');
+  console.log(handele);
+  const visits =() =>{
+    // setvisit(true)
+    if(visit){
+      setvisit(false)
+
+    }
+    else{
+      setvisit(true)
+    };
+    handele(Countryname)
     
   }
 
   return (
-    <div className ='country'>
+    <div className ={`country ${visit && 'country-visited' }`}>
        <h3>Name:{Countryname.name.common}</h3>
        <img src={Countryname.flags.flags.png} alt={Countryname.flags.flags.alt}></img>
        <p>continents:{Countryname.continents.continents}</p>
@@ -17,7 +32,10 @@ const CountryName = ({Countryname}) => {
        <p>population:{Countryname.population.population}</p>
        <p>area:{Countryname.area.area} 
         {Countryname.area.area > 3000 ? "Big Crounty" :"Small crounty"}</p>
-        <button onClick={visit}>Not visited</button>
+        <button onClick={visits}>
+          
+           {visit ? 'dukci':'duki nai'}
+        </button>
     </div>
   );
 };
